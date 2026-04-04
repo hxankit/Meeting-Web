eb7d2259d46e0adb06919deb1a675e17ceb52a94
Ankit Verma <devxanki@gmail.com>
2026-04-04 18:18:25 +0530

    Working on env

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,2 +1,7 @@
-const server = process.env.REACT_APP_API_URL || "/api";
+let server = "/api";
+if (process.env.NODE_ENV === "development") {
+	server = "http://localhost:8000";
+} else if (process.env.REACT_APP_API_URL) {
+	server = process.env.REACT_APP_API_URL;
+}
 export default server;
